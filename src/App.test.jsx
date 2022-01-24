5da5f94f95d0b817e94f7253edfb3036e9c643ec
Chase Crowder <gcrowder93@gmail.com>
2022-01-24 14:15:18 -0800

    app tests are passing and showing header, profile name, and image

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+  render(<App />)
 
+  const heading = screen.getByAltText(/Alchemy Logo/i)
+  const name = screen.getByText(/meet/i)
+  const actname = await screen.findByText(/vonta/i)
+
+  expect(heading).toBeInTheDocument()
+  expect(name).toBeInTheDocument()
+  expect(actname).toBeInTheDocument()
 })
